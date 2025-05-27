1616ca3b72625c74cb493995a6f8d5946f29f2e5
yrichtermeier <141412736+yrichtermeier@users.noreply.github.com>
2025-05-27 14:44:52 +0200

    Switch states by pressing buttons

--- segmentanzeige.py
+++ segmentanzeige.py
@@ -1,21 +1,26 @@
 from general_functions import *
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_dual_button_v2 import BrickletDualButtonV2
 
-def show_temperature():
-    HOST = "172.20.10.242"
-    PORT = 4223
+HOST = "172.20.10.242"
+PORT = 4223
+UID = "Vd8"
+index = 1
+ipcon = IPConnection()
+db = BrickletDualButtonV2(UID, ipcon)
+
+def show_temperature(): # C
     UIDT = "Wcg"
 
-    from tinkerforge.ip_connection import IPConnection
     from tinkerforge.bricklet_ptc_v2 import BrickletPTCV2
 
-    ipcon = IPConnection()
+    global ipcon
     ptc = BrickletPTCV2(UIDT, ipcon)
-
-    ipcon.connect(HOST, PORT)
 
     temperature = ptc.get_temperature()
     temperature = int(temperature/100.0)
     temperature_array = zerlege_zahl_in_ziffern(temperature)
+    print("temp")
 
     UIDS = "Tre"
 
@@ -26,26 +31,24 @@ def show_temperature():
     sd.set_brightness(10)
 
     sd.set_numeric_value(temperature_array)
+    sd.set_selected_segment(1, False)
+    sd.set_selected_segment(2, False)
+    sd.set_selected_segment(6, False)
 
-    ipcon.disconnect()
 
 
-
-def show_illuminance():
-    HOST = "172.20.10.242"
-    PORT = 4223
+def show_illuminance(): # L
     UID = "Pdw" 
 
-    from tinkerforge.ip_connection import IPConnection
     from tinkerforge.bricklet_ambient_light_v3 import BrickletAmbientLightV3
 
-    ipcon = IPConnection()
+    global ipcon
     al = BrickletAmbientLightV3(UID, ipcon) 
 
-    ipcon.connect(HOST, PORT)
     illuminance = al.get_illuminance()
     illuminance = int(illuminance/100.0)
     illuminance_array = zerlege_zahl_in_ziffern(illuminance)
+    print("illu")
 
     UIDS = "Tre"
 
@@ -56,28 +59,25 @@ def show_illuminance():
     sd.set_brightness(10)
 
     sd.set_numeric_value(illuminance_array)
+    sd.set_selected_segment(0, False)
+    sd.set_selected_segment(1, False)
+    sd.set_selected_segment(2, False)
+    sd.set_selected_segment(6, False)
 
-    ipcon.disconnect()
 
 
-
-def show_humidity():
-    HOST = "172.20.10.242"
-    PORT = 4223
+def show_humidity(): # P
     UID = "ViW"
 
-    from tinkerforge.ip_connection import IPConnection
     from tinkerforge.bricklet_humidity_v2 import BrickletHumidityV2
 
-    ipcon = IPConnection()
+    global ipcon
     h = BrickletHumidityV2(UID, ipcon)
 
-    ipcon.connect(HOST, PORT)
-
     humidity = h.get_humidity()
-    print("Humidity: " + str(humidity/100.0) + " %RH")
     humidity = int(humidity/100.0)
     humidity_array = zerlege_zahl_in_ziffern(humidity)
+    print("humi")
 
     UIDS = "Tre"
 
@@ -88,57 +88,65 @@ def show_humidity():
     sd.set_brightness(10)
 
     sd.set_numeric_value(humidity_array)
+    sd.set_selected_segment(2, False)
+    sd.set_selected_segment(3, False)
+    sd.set_selected_segment(6, True)
 
-    ipcon.disconnect()
 
-HOST = "172.20.10.242"
-PORT = 4223
-UID = "Vd8"
-
-index = 0
-order = [1, 2, 3]
 
 def cb_state_changed(button_l, button_r, led_l, led_r):
     from tinkerforge.bricklet_dual_button_v2 import BrickletDualButtonV2
 
     global index
+    global db
 
-    if button_l == BrickletDualButtonV2.BUTTON_STATE_RELEASED:
-        index = (index - 1) % 3
-        if order[index] == 1:
-            show_temperature()
-        elif order[index] == 2:
-            show_illuminance()
-        elif order[index] == 3:
-            show_humidity()
-        print("Left Button gedrückt")
+    if button_l == BrickletDualButtonV2.BUTTON_STATE_PRESSED:
+        db.set_led_state(2, 3)
+        if index == 1:
+            index = 3
+        else:
+            index = index - 1
 
-    if button_r == BrickletDualButtonV2.BUTTON_STATE_RELEASED:
-        index = (index + 1) % 3
-        if order[index] == 1:
+        if index == 1:
             show_temperature()
-        elif order[index] == 2:
+        elif index == 2:
             show_illuminance()
-        elif order[index] == 3:
+        elif index == 3:
             show_humidity()
-        print("Right Button gedrückt")
+        print("Left Button gedrückt", index)
+
+    elif button_r == BrickletDualButtonV2.BUTTON_STATE_PRESSED:
+        db.set_led_state(3, 2)
+        if index == 3:
+            index = 1
+        else:
+            index = index + 1
+
+        if index == 1:
+            show_temperature()
+        elif index == 2:
+            show_illuminance()
+        elif index == 3:
+            show_humidity()
+        print("Right Button gedrückt", index)
 
 def buttons():
+    global ipcon
+    ipcon.connect(HOST, PORT)
+
     show_temperature()
 
-    from tinkerforge.ip_connection import IPConnection
     from tinkerforge.bricklet_dual_button_v2 import BrickletDualButtonV2
 
-    ipcon = IPConnection()
-    db = BrickletDualButtonV2(UID, ipcon)
+    global db
 
-    ipcon.connect(HOST, PORT)
+    db = BrickletDualButtonV2(UID, ipcon)
 
     db.register_callback(db.CALLBACK_STATE_CHANGED, cb_state_changed)
     db.set_state_changed_callback_configuration(True)
 
     print("Warte auf Tastendrücke... (zum Beenden Strg+C drücken)")
-    input()  # hält das Programm offen
+    input()
 
     ipcon.disconnect()
 
